45e80281cd7ee889c21aa68a55cf4badf8886906
Ji Yim <gsooyim@gmail.com>
2018-04-19 03:45:08 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,9 +31,7 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] => 2
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest["muskets"] => 2
-
-
+  shipping_manifest["gun powder"] => 4
   # return the shipping_manifest hash below
   shipping_manifest
 end
